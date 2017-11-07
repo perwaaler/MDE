a510483265df799d0e668aae3bc2a76c3aba420c
perwaaler <perwaaler@gmail.com>
2017-11-07 10:26:28 +0100

    123
    jag gjorde adpt bla

---
+++ adaptiveRK34.m
@@ -0,0 +1,26 @@
+function [t_list,y_list] = adaptiveRK34(f, y0, t0, tf, tol)
+
+
+k=4                                               %order of method is 4
+y_list=y0;
+t_list=[t0];
+rold=tol;
+hold=abs(tf-t0)*tol^.25/(100*(1+norm(test2(t0,y0)) )); %defining h0
+h_list=[hold];
+
+while t_list(end)+hold < tf
+    [y, rnew] = RK34step(f, y_list(:,end), t_list(end), hold);
+    rnew      = norm(rnew);                         %new local error
+    t_list    = cat(2,t_list,t_list(end)+hold);     %update time
+    hnew      = newstep(tol, rnew, rold, hold,k);   %size of next time-step
+    y_list    = [y_list,y];                         %add newest y value
+    h_list    = cat(2,h_list,hnew);                 
+    rold      = rnew;
+    hold      = hnew;
+    
+end
+
+hf=tf-t_list(end);                                  %length of last interval
+yf = RK34step(f, y_list(:,end), t_list(end), hf);     %value of y at tf
+t_list=cat(2,t_list,tf);                            
+y_list=[y_list,yf];
